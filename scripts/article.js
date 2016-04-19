87618679890c4c838a9f7b1f12f5365265063c28
Yvonne Hayes <queyvonne@gmail.com>
2016-04-19 12:50:44 -0700

    once more. Not sure it worked before

--- scripts/article.js
+++ scripts/article.js
@@ -1,5 +1,5 @@
-var articles = [];
-
+// TODO: Wrap the entire contents of this file in an IIFE.
+// Pass in to the IIFE a module, upon which objects can be attached for later access.
 function Article (opts) {
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
@@ -9,35 +9,83 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
 }
 
+Article.all = [];
+
 Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.removeClass('template');
-  if (!this.publishedOn) {
-    $newArticle.addClass('draft');
+  var template = Handlebars.compile($('#article-template').text());
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  this.body = marked(this.body);
+
+  return template(this);
+};
+
+Article.loadAll = function(rawData) {
+  rawData.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
+
+  // DONE: Refactor this forEach code, by using a `.map` call instead, since what we are
+  // trying to accomplish is the transformation of one colleciton into another.
+
+  // rawData.forEach(function(ele) {
+  //   Article.all.push(new Article(ele));
+  // })
+  Article.all = rawData.map(function(ele) {
+    return new Article(ele);
+  });
+};
+
+// This function will retrieve the data from either a local or remote source,
+// and process it, then hand off control to the View.
+
+// TODO: Refactor this function, and provide it with a parameter of a callback function
+//(for now just a placeholder, but to be referenced at call time as a view function)
+// to execute once the loading of articles is done. We do this because we might want
+// to call other view functions, and not just the initIndexPage() that we are replacing.
+// Now, instead of calling articleView.initIndexPage(), we can simply run our callback.
+Article.fetchAll = function() {
+  if (localStorage.rawData) {
+    Article.loadAll(JSON.parse(localStorage.rawData));
+    articleView.initIndexPage();
+  } else {
+    $.getJSON('/data/hackerIpsum.json', function(rawData) {
+      Article.loadAll(rawData);
+      localStorage.rawData = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
+      articleView.initIndexPage();
+    });
   }
-  $newArticle.attr('data-category', this.category);
-  // TODO: Use jQuery to also add the author name as a data-attribute of the newly cloned article.
-  //       Doing so will allow us to use selectors to target articles, based on who wrote them.
+};
 
-  $newArticle.find('.byline a').html(this.author);
-  $newArticle.find('.byline a').attr('href', this.authorUrl);
-  $newArticle.find('h1:first').html(this.title);
-  $newArticle.find('.article-body').html(this.body);
-  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn)
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
-  $newArticle.append('<hr>');
-  return $newArticle;
-}
+// TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+Article.numWordsAll = function() {
+  return Article.all.map(function(article) {
+    return whatShouldIReturn; // Grab the words from the `article` `body`.
+  })
+  .reduce(function(a, b) {
+    return whatShouldIReturn;// Sum up all the values!
+  });
+};
 
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
+// TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names.
+Article.allAuthors = function() {
+  // Read docs on .map and .reduce! You can reference the previous
+  // `map` in the numWordsAll method to get started here.
 
-rawData.forEach(function(ele) {
-  articles.push(new Article(ele));
-})
+  // For our `reduce` -- since we are trying to return an array, we'll need to specify an accumulator type...
+  // what data type should this accumulator be and where is it placed?
+  return whatShouldIReturn;
+};
 
-articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
-});
+Article.numWordsByAuthor = function() {
+  // TODO: Transform each author string into an object with 2 properties: One for
+  // the author's name, and one for the total number of words across the matching articles
+  // written by the specified author.
+  return Article.allAuthors().map(function(author) {
+    return {
+      // name:
+      // numWords: someCollection.someArrayMethod().map(...).reduce(...), ...
+    };
+  });
+};
