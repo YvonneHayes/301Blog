64b5622476a5d07a195485dd5c8798b8a497bace
Randy Gylling <MikeGylling@yahoo.com>
2016-04-19 17:01:44 -0700

    finished numWords function

--- scripts/article.js
+++ scripts/article.js
@@ -79,7 +79,7 @@
   };
 
 
-// TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names.
+// DONE: Chain together a `map` and a `reduce` call to produce an array of unique author names.
   Article.allAuthors = function() {
     return Article.all.map(function(article) {
       return (article.author); // Grab the words from the `article` `body`.
@@ -100,13 +100,21 @@
 
 
   Article.numWordsByAuthor = function() {
-  // TODO: Transform each author string into an object with 2 properties: One for
+  // Done: Transform each author string into an object with 2 properties: One for
   // the author's name, and one for the total number of words across the matching articles
   // written by the specified author.
     return Article.allAuthors().map(function(author) {
       return {
-      // name:
-      // numWords: someCollection.someArrayMethod().map(...).reduce(...), ...
+        name: author,
+        numWords: Article.all.map(function (article) {
+          if (article.author === author){
+            return wordCount(article.body);
+          } else {
+            return 0;
+          }
+        }).reduce(function(a, b) {
+          return a+b;
+        })
       };
     });
   };
