482dde83654127c1eca1c224750784d889db96f0
Yvonne Hayes <queyvonne@gmail.com>
2016-04-25 13:24:29 -0700

    on aboutcontroller hid main and showed about

--- scripts/aboutController.js
+++ scripts/aboutController.js
@@ -1,9 +1,10 @@
 (function(module) {
   var aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
-
+    $('main > section').hide();
+    $('#about').fadeIn();
   };
 
   module.aboutController = aboutController;
