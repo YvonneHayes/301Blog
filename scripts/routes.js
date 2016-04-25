6ab324032ddecb7fecdca067ff4f844f6639965e
Yvonne Hayes <queyvonne@gmail.com>
2016-04-25 13:07:01 -0700

    called page() function

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,4 +1,5 @@
 // TODO: Configure our two routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do we call to activate page.js? Fire it off now, to execute
+// DONE: What function do we call to activate page.js? Fire it off now, to execute
+page();
