818d02a5476be05aedea2980f846d84a44db6b3f
Yvonne Hayes <queyvonne@gmail.com>
2016-04-25 13:15:11 -0700

    added iife to routes and made controller and routing functions

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,5 +1,19 @@
-// TODO: Configure our two routes for this app with page.js, by registering each URL your app can handle,
+// DONE: Configure our two routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// DONE: What function do we call to activate page.js? Fire it off now, to execute
-page();
+(function(module){
+  var routes = {};
+
+  routes.setRouteMappings = function() {
+    page.base('/');
+
+    page('', articlesController.index);
+    page('about', aboutController.index);
+
+    // DONE: What function do we call to activate page.js? Fire it off now, to execute
+    page();
+  };
+
+  module.setRouteMappings = setRouteMappings; //making it global
+
+})(window);
