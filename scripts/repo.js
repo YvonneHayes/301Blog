57f3516b1d57b74ea2f5db826b9b0939406b2b9a
Yvonne Hayes <queyvonne@gmail.com>
2016-04-27 09:23:52 -0700

    added starter code to new branch

---
+++ scripts/repo.js
@@ -0,0 +1,27 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
+  repos.requestRepos = function(callback) {
+    $.ajax({
+      url: 'https://api.github.com/users/brookr/repos' +
+            '?per_page=50' +
+            '&sort=updated',
+      type: 'GET',
+      headers: { 'Authorization': 'token ' + githubToken },
+      success: function(data, message, xhr) {
+        repos.all = data;
+      }
+    }).done(callback);
+  };
+
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
---
+++ scripts/routes.js
@@ -0,0 +1,4 @@
+page('/', articlesController.index);
+page('/about', aboutController.index);
+
+page();
