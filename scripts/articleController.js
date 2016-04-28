3c48b917d47849e7900683c65dc6b336c54dbafe
Yvonne Hayes <queyvonne@gmail.com>
2016-04-28 15:36:57 -0700

    started on question 2

--- scripts/articleController.js
+++ scripts/articleController.js
@@ -11,7 +11,7 @@
   // COMMENT: What does this method do?  What is it's execution path?
 
   //loadById gets executed when URL contains /aricle/:id.
-  //routes.js calls page.js library and activates and calls articlesController which defines the function articlesData. FindWhere runs a SQL query by id and calls articleData with that returned id. 
+  //routes.js calls page.js library and activates and calls articlesController which defines the function articlesData. FindWhere runs a SQL query by id and calls articleData with that returned id.
 
   articlesController.loadById = function(ctx, next) {
     var articleData = function(article) {
@@ -23,7 +23,7 @@
     Article.findWhere('id', ctx.params.id, articleData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // FindWhere is defined in article.js line 94. It generates a SQL query that selects everything from the articles table with a conditional parameter ('field') and then calls a function using the results of the query. Anytime an ariclesController methos is called by page it goes back to the findWhere query and then follows through the logic using the results of the query (in the given ariclerController method). Line 27 is one such method that will use the results of the query in line 33. For the user it displays aricles by a chosen author.
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
